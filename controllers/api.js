3ef4a8c242788af6fa7be73ecac1a9d37089c07c
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-08-16 17:50:07 -0400

    added routes

--- controllers/api.js
+++ controllers/api.js
@@ -1,20 +1,59 @@
 const router = require('express').Router();
-const Workout = require('../models/workout');
+const Workout = require('../models/Workout');
 
-router.post('/workouts', (req, res) => {
 
+router.post('/workouts', ({body}, res) => {
+    const workout = body;
+    Workout.create(workout)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 })
 
 router.put('/workouts/:id', ({body, params}, res) => {
-
+    Workout.findOneAndUpdate(
+        {
+            _id: params.id
+        },
+        {
+            $push: {
+                exercises: body
+            }
+        },
+        {
+            new: true
+        }
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+    )
 })
 
 router.get('/workouts', (req, res) => {
-
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        })
 })
 
 router.get('/workouts/range', (req, res) => {
-
+    Workout.find({})
+        .limit(7)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        })
 })
 
 module.exports = router;
--- models/workout.js
+++ models/workout.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
 
-const workoutSchema = new workoutSchema({
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
     },
-    exercises: {
-
-    },
-
+    exercises: [
+        {
+          type: String,
+          name: String,
+          duration: Number,
+          weight: Number,
+          reps: Number,
+          sets: Number,
+          distance: Number,
+        },
+      ]
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);

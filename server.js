bb53592889c497216b626df203c5b4f2739fecb3
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-08-17 17:18:06 -0400

    deploying

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // connecction to database
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,

c03f2f8f537ba43076ab8e357425b1cffac85fcd
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-08-18 09:18:36 -0400

    connection

--- server.js
+++ server.js
@@ -23,8 +23,9 @@ app.use(express.static('public'));
 // connecction to database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 // routes
